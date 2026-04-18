e7ff1880534c4b9e28bfd6aef4cd445cc472f9e0
Guilherme Fernandes <guiiilherme.fernandes@gmail.com>
2026-04-18 18:38:18 -0300

    feature: whole project routes on 'routes' folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,15 @@
 import './App.css';
 
-// permite o react ler varias paginas
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import { Home } from './pages/user/Home.jsx';
 import { Login } from './features/auth/Login.jsx';
 import { Register } from './features/auth/Register.jsx';
 // Lembre-se de importar o ProductDetails, Dashboard e Inventory também! (perguntar oq é pra IA)
-import { UserLayout } from './layouts/UserLayout';
-import { AdminLayout } from './layouts/AdminLayout';
+import { AppRoutes } from './routes';
 
 function App() {
   return (
-      <BrowserRouter>
-        <Routes>
-          {/* USER */}
-          <Route element= { <UserLayout /> }>
-            <Route path='/' element= { <Home /> } />
-            <Route path='/product/:id' /* element= { <ProductDetails/>  }*/ />
-            <Route path='/login' element= { <Login /> } />
-            <Route path='/register' element= { <Register /> } />
-          </Route>
-        
-        {/*
-        ADMIN
-          <Route element= { <AdminLayout /> }> 
-            <Route index element= { <Dashboard /> } />
-            <Route path='products' element= { <Inventory /> } /> 
-          </Route> 
-        */}
-        </Routes> 
-
-      </BrowserRouter>
+      <AppRoutes />
   )
 }
 
---
+++ src/routes/index.jsx
@@ -0,0 +1,34 @@
+// permite o react ler varias paginas
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+import { UserLayout } from '../layouts/UserLayout'
+import { AdminLayout } from '../layouts/AdminLayout'
+import { Home } from '@/pages/user/Home'
+import { Login } from '@/features/auth/Login'
+import { Register } from '@/features/auth/Register'
+
+
+export function AppRoutes() {
+    return (
+        <BrowserRouter>
+            <Routes>
+            {/* USER */}
+            <Route element= { <UserLayout /> }>
+                <Route path='/' element= { <Home /> } />
+                <Route path='/product/:id' /* element= { <ProductDetails/>  }*/ />
+                <Route path='/login' element= { <Login /> } />
+                <Route path='/register' element= { <Register /> } />
+            </Route>
+            
+            {/*
+            ADMIN
+            <Route element= { <AdminLayout /> }> 
+                <Route index element= { <Dashboard /> } />
+                <Route path='products' element= { <Inventory /> } /> 
+            </Route> 
+            */}
+            </Routes> 
+
+        </BrowserRouter>
+    )
+}

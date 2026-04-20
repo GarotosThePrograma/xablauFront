8a57c24573a412315e8ea44c1e341c54dbf46f3d
guifernands <guiiilherme.fernnades@gmail.com>
2026-04-20 09:07:56 -0300

    refactor(home): move product array to features/products

--- src/pages/user/Home.jsx
+++ src/pages/user/Home.jsx
@@ -4,30 +4,7 @@ import { Box, Flex, Text, Image, Button, Icon, Span } from '@chakra-ui/react';
 import { BsCart3, BsCheckLg } from 'react-icons/bs';
 import { useState } from 'react';
 import { useCartStore } from '../../store/useCartStore';
-
-const products = [
-  {
-    id: 1,
-    name: 'Placa de Vídeo Challenger ASRock AMD Radeon RX 7600 8GB',
-    price: 'R$ 1.996,65',
-    stock: 0,
-    img: 'https://images4.kabum.com.br/produtos/fotos/907814/placa-de-video-asrock-amd-radeon-rx-7600-challenger-pro-oc-8gb-gddr6-triple-fan-90-ga62zz-00uanf_1761582764_gg.jpg',
-  },
-  {
-    id: 2,
-    name: 'Placa De Vídeo Husky Alpha RX 580 AMD 8GB GDDR5',
-    price: 'R$ 899,99',
-    stock: 10,
-    img: 'https://images1.kabum.com.br/produtos/fotos/692461/placa-de-video-husky-alpha-rx-580-amd-8gb-gddr5-256-bit-hdmi-dvi-displayport-hvg580_1773838121_gg.jpg',
-  },
-  {
-    id: 3,
-    name: 'Placa de Vídeo ASRock RX 6600 Challenger White AMD Radeon 8GB',
-    price: 'R$ 1.499,99',
-    stock: 5,
-    img: 'https://images7.kabum.com.br/produtos/fotos/695107/placa-de-video-asrock-rx-6600-challenger-white-amd-radeon-8gb-gddr6-directx-12-ultimate-rdna-2-90-ga4uzz-00uanf_1742841360_gg.jpg',
-  },
-];
+import { products } from '../../features/products/Products';
 
 function ProductCard({ product }) {
   const [added, setAdded] = useState(false);
@@ -39,6 +16,8 @@ function ProductCard({ product }) {
     setAdded(true);
     setTimeout(() => setAdded(false), 1000);
   };
+
+  
 
   return (
     <Flex
